b960b612ca50eede25ba48757589c31d40675358
Destry2xd <josegpadillac08@gmail.com>
2025-05-31 23:54:11 -0400

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Serviaires' });
+});
+
+router.get('/formulario', function(req, res, next) {
+  res.render('Formulario', { title: 'Comentarios' });
+});
+
+router.get('/precios', function(req, res, next) {
+  res.render('Precios', { title: 'Precios' });
+});
+
+router.get('/productos', function(req, res, next) {
+  res.render('Productos', { title: 'Productos' });
+});
+
+router.get('/formulario_pago', function(req, res, next) {
+  res.render('Formulario_pago', { title: 'Pagos' });
+});
+
+router.get('/exito', function(req, res, next) {
+  res.render('Exito', { title: 'Pago realizado con exito' });
+});
+
+
+module.exports = router;
